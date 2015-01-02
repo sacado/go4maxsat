0a69d0e49ebbfc078c957e6df505f149add05a20
Fabien Delorme <fabien.delorme@gmail.com>
2015-01-02 22:12:32 +0100

    added comment

--- solver/solver.go
+++ solver/solver.go
@@ -1,5 +1,7 @@
 package solver
 
+// A Solver is a type that can solve a problem and return a Model
+// that satisfies as many clauses as possible.
 type Solver interface {
 	Solve(nbTries int) Model
 	Score() int // Current score, i.e. total weight of unsat clauses
